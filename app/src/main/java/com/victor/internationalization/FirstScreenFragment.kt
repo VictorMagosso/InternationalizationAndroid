a1771b3e3d6ee36368aa6aecca6b52f1e794579f
Victor Magosso <victor_magosso@hotmail.com>
2021-06-17 23:02:24 -0300

    initial screen

---
+++ app/src/main/java/com/victor/internationalization/FirstScreenFragment.kt
@@ -0,0 +1,23 @@
+package com.victor.internationalization
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class FirstScreenFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_first_screen, container, false)
+
+        view.findViewById<Button>(R.id.btnNext).setOnClickListener{
+
+        }
+        return view
+    }
+}
